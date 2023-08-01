251500829f04ac349fcc2d16c8d0fd4058f30d13
Jesse Carson <jmcarson222@gmail.com>
2023-08-01 09:48:41 -0500

    server setup w/ express

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("server working");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
+});
+
+app.listen(5100, () => {
+  console.log("Server listening on port 5100...");
+});
