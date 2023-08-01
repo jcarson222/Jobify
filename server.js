84b82ebe4bdf9ec1b9078d382cf4091ee8601df6
Jesse Carson <jmcarson222@gmail.com>
2023-08-01 10:23:12 -0500

    env variables & morgan

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 app.post("/", (req, res) => {
-  console.log(req);
+  // console.log(req);
 
   res.json({ message: "Data received", data: req.body });
 });
 
-app.listen(5100, () => {
-  console.log("Server listening on port 5100...");
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
 });
