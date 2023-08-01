89f31fed8b07409013c7c3712d1958d9c613dbee
Jesse Carson <jmcarson222@gmail.com>
2023-08-01 14:12:17 -0500

    set up mongoDB connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
-import { nanoid } from "nanoid";
+import mongoose from "mongoose";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
@@ -25,8 +25,23 @@ app.post("/", (req, res) => {
 
 app.use("/api/v1/jobs", jobRouter);
 
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
 const port = process.env.PORT || 5100;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
