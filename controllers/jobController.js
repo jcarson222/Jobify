190f1242f113734478e4003f9ec8987d2af28cbf
Jesse Carson <jmcarson222@gmail.com>
2023-08-01 11:07:40 -0500

    single job and all jobs routes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,9 +1,19 @@
+import { jobs } from "../server.js";
+
 export const getAllJobs = async (req, res) => {
-  res.json("get all jobs");
+  res.status(200).json({ jobs });
 };
 
 export const getJob = async (req, res) => {
-  res.json("get single job");
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  res.status(200).json({ job });
 };
 
 export const createJob = async (req, res) => {
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import { nanoid } from "nanoid";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
+
+// test jobs
+export let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
 
 const app = express();
 app.use(express.json());
