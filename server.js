d6cc04d8a0f2c88fa4105a279087c326ca06d597
Jesse Carson <jmcarson222@gmail.com>
2023-08-08 14:10:27 -0500

    use authRouter

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -21,6 +22,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/jobs", jobRouter);
+
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
