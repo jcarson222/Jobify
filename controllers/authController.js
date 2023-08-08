4bc63b29607dcf168990c0a79d109f997deb1c75
Jesse Carson <jmcarson222@gmail.com>
2023-08-08 14:16:50 -0500

    register user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
+import { StatusCodes } from "http-status-codes";
+import User from "../models/UserModel.js";
+
 export const login = (req, res) => {
   res.send("login");
 };
 
-export const register = (req, res) => {
-  res.send("register");
+export const register = async (req, res) => {
+  const user = await User.create(req.body);
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ msg: `Welcome ${user.name}!`, user: user });
 };
