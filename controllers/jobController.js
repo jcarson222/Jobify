80be95433b3b6d118bde8de5c7a3ae21899a6d85
Jesse Carson <jmcarson222@gmail.com>
2023-08-10 14:39:27 -0500

    req user & verify token

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -3,7 +3,7 @@ import Job from "../models/JobModel.js";
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
 
   res.status(StatusCodes.OK).json({ jobs });
 };
@@ -19,9 +19,9 @@ export const getJob = async (req, res) => {
 
 // CREATE JOB
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
+  req.body.createdBy = req.user.userId;
 
-  const job = await Job.create({ company, position });
+  const job = await Job.create(req.body);
 
   res.status(StatusCodes.CREATED).json({ msg: "job created", job });
 };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,20 @@
 import { UnauthenticatedError } from "../errors/customErrors.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
   // ^^^ access to cookie from cookieParser in server.js
+  // console.log(req.cookies);
 
   if (!token) {
     throw new UnauthenticatedError("authentication invalid");
   }
-  // console.log(req.cookies);
 
-  next();
+  try {
+    const { userId, role } = verifyJWT(token);
+    req.user = { userId, role };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
 };
