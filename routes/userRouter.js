c66f6b445477c5fcfb3909c0535c5af3e2224d0a
Jesse Carson <jmcarson222@gmail.com>
2023-08-11 09:58:32 -0500

    authorize permissions

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,8 @@ const router = Router();
 
 import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
 
+import { authorizePermissions } from "../middleware/authMiddleware.js";
+
 import {
   getCurrentUser,
   getApplicationStats,
@@ -10,7 +12,9 @@ import {
 } from "../controllers/userController.js";
 
 router.route("/current-user").get(getCurrentUser);
-router.route("/admin/app-stats").get(getApplicationStats);
+router
+  .route("/admin/app-stats")
+  .get(authorizePermissions("admin"), getApplicationStats);
 router.route("/update-user").patch(validateUpdateUserInput, updateUser);
 
 export default router;
