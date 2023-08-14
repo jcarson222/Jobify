6abf6e7e0e3860bb422395eb2af3c657186e6eb2
Jesse Carson <jmcarson222@gmail.com>
2023-08-14 12:10:06 -0500

    remove avatar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import {
   HomeLayout,
@@ -15,24 +15,24 @@ import {
   Profile,
   Admin,
   EditJob,
-} from './pages';
+} from "./pages";
 
-import { action as registerAction } from './pages/Register';
-import { action as loginAction } from './pages/Login';
-import { loader as dashboardLoader } from './pages/DashboardLayout';
-import { action as addJobAction } from './pages/AddJob';
-import { loader as allJobsLoader } from './pages/AllJobs';
-import { loader as editJobLoader } from './pages/EditJob';
-import { action as editJobAction } from './pages/EditJob';
-import { action as deleteJobAction } from './pages/DeleteJob';
-import { loader as adminLoader } from './pages/Admin';
-import { action as profileAction } from './pages/Profile';
-import { loader as statsLoader } from './pages/Stats';
-import ErrorElement from './components/ErrorElement';
+import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
+import { loader as dashboardLoader } from "./pages/DashboardLayout";
+import { action as addJobAction } from "./pages/AddJob";
+import { loader as allJobsLoader } from "./pages/AllJobs";
+import { loader as editJobLoader } from "./pages/EditJob";
+import { action as editJobAction } from "./pages/EditJob";
+import { action as deleteJobAction } from "./pages/DeleteJob";
+import { loader as adminLoader } from "./pages/Admin";
+import { action as profileAction } from "./pages/Profile";
+import { loader as statsLoader } from "./pages/Stats";
+import ErrorElement from "./components/ErrorElement";
 
 const checkDefaultTheme = () => {
-  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
-  document.body.classList.toggle('dark-theme', isDarkTheme);
+  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  document.body.classList.toggle("dark-theme", isDarkTheme);
   return isDarkTheme;
 };
 
@@ -48,7 +48,7 @@ const queryClient = new QueryClient({
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
@@ -57,17 +57,17 @@ const router = createBrowserRouter([
         element: <Landing />,
       },
       {
-        path: 'register',
+        path: "register",
         element: <Register />,
         action: registerAction,
       },
       {
-        path: 'login',
+        path: "login",
         element: <Login />,
-        action: loginAction(queryClient),
+        action: loginAction,
       },
       {
-        path: 'dashboard',
+        path: "dashboard",
         element: (
           <DashboardLayout
             isDarkThemeEnabled={isDarkThemeEnabled}
@@ -82,34 +82,34 @@ const router = createBrowserRouter([
             action: addJobAction(queryClient),
           },
           {
-            path: 'stats',
+            path: "stats",
             element: <Stats />,
             loader: statsLoader(queryClient),
             errorElement: <ErrorElement />,
           },
           {
-            path: 'all-jobs',
+            path: "all-jobs",
             element: <AllJobs />,
             loader: allJobsLoader(queryClient),
             errorElement: <ErrorElement />,
           },
           {
-            path: 'profile',
+            path: "profile",
             element: <Profile />,
             action: profileAction(queryClient),
           },
           {
-            path: 'admin',
+            path: "admin",
             element: <Admin />,
             loader: adminLoader,
           },
           {
-            path: 'edit-job/:id',
+            path: "edit-job/:id",
             element: <EditJob />,
             loader: editJobLoader(queryClient),
             action: editJobAction(queryClient),
           },
-          { path: 'delete-job/:id', action: deleteJobAction(queryClient) },
+          { path: "delete-job/:id", action: deleteJobAction(queryClient) },
         ],
       },
     ],
